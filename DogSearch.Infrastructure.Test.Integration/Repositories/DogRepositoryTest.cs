e8944a02ddef5fbdff8d861402aada7ad3b91423
Stuart Sam Warren <sswarren1995@gmail.com>
2024-01-20 10:39:53 +0100

    Add test projects.

---
+++ DogSearch.Infrastructure.Test.Integration/Repositories/DogRepositoryTest.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DogSearch.Infrastructure.Test.Integration.Repositories;
+
+public class DogRepositoryTest
+{
+}
